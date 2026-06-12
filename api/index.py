4b752c8ce5ce930da365821b5f929a2f299574a1
AvGeek01 <majestictiger0106@gmail.com>
2026-06-12 18:13:44 +0400

    Delete api/index.py

--- api/index.py
+++
@@ -1,8 +0,0 @@
-import sys
-import os
-
-# Add the root directory to the python path so we can import our app
-root_dir = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-sys.path.append(root_dir)
-
-from app import app
